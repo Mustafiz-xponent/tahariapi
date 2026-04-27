19b102eed9fd51c4bf0e8866730a9880d7455f3b
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2026-04-27 18:20:10 +0600

    fix: remove module-alias, use tsconfig-paths only

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,14 @@
+import * as tsConfigPaths from "tsconfig-paths";
+import * as tsConfig from "../../tsconfig.json";
+
+tsConfigPaths.register({
+  baseUrl: ".",
+  paths: tsConfig.compilerOptions.paths,
+});
+
 import dotenv from "dotenv";
 dotenv.config();
 
-import app from "../app"; //
+import app from "../app";
+
 export default app;
