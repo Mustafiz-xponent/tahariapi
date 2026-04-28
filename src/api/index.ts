a1af1d8e13b57599d2be616a589f51ad16a06017
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2026-04-28 09:32:19 +0600

    fix: compile TS to dist and serve from dist on Vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,6 @@
-import path from "path";
-
-// Must run before any imports that use @/ aliases
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const moduleAlias = require("module-alias");
-moduleAlias.addAlias("@", path.join(__dirname, ".."));
-
-// Use require instead of import so alias is applied first
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const dotenv = require("dotenv");
+import dotenv from "dotenv";
 dotenv.config();
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const app = require("../app").default;
+import app from "../app";
 
-module.exports = app;
+export default app;
